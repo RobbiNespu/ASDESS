a6f477f4ae8ca9e6481d8359190763e6de80b02c
Robbi Nespu <robbinespu@gmail.com>
2021-05-17 15:53:32 +0800

    remove, let do it again

--- src/main/java/com/github/robbinespu/asdess/exceptions/BusinessIntegrityException.java
+++
@@ -1,7 +0,0 @@
-package com.github.robbinespu.asdess.exceptions;
-
-public class BusinessIntegrityException extends Exception {
-    public BusinessIntegrityException(String message) {
-        super(message);
-    }
-}
